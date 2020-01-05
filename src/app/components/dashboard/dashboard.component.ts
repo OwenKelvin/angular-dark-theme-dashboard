76915105bf8d8b4d4cc8532eff2a1bbd23d2f518
Owen Kelvin <otienoowenkelvin@gmail.com>
2020-01-05 19:30:43 +0300

    added tel inpuy

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import * as fromStore from '../../store/reducers';
+import { AppState } from '../../store/reducers';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,10 +9,15 @@ import * as fromStore from '../../store/reducers';
   styleUrls: ['./dashboard.component.less']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor(private store: Store<fromStore.AppState>) { }
+  exampleForm: FormGroup;
+  triggerValidation: boolean;
+  constructor(private store: Store<AppState>, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.exampleForm = this.fb.group({
+      phone: ['']
+    });
+    this.exampleForm.get('phone').valueChanges.subscribe(item => console.log(this.exampleForm.get('phone')));
   }
 
 }
